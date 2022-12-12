4a89ff7de3227de64799337738f9dd4767bf66f6
Lina Hu <lina.hu@cbc.ca>
2022-12-11 21:33:12 -0500

    initial React and Firebase setup

---
+++ client/src/utils/firebase.js
@@ -0,0 +1,24 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbqP-BT360rl0g8QdmEbSrjh_my3S2F6M",
+  authDomain: "flowtree-1fe96.firebaseapp.com",
+  projectId: "flowtree-1fe96",
+  storageBucket: "flowtree-1fe96.appspot.com",
+  messagingSenderId: "845788049765",
+  appId: "1:845788049765:web:f1e851eb1154013d9b318c"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+
+export default firebase;
